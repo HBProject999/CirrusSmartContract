ac4556c55b42a02708f9cd9dd63ed4a14a220181
Manish Kumar <manish.kumar@mandeeps.com>
2022-03-01 19:01:07 +0530

    Add files via upload

--- Arena.cs
+++ Arena.cs
@@ -2,6 +2,9 @@
 using System;
 using System.Text;
 
+/// <summary>
+/// A Stratis smart contract for running a game battle where owner will start the battle and maximum 4 users can enter a battle
+/// </summary>
 public class Arena : SmartContract
 {
     public Arena(ISmartContractState smartContractState)
@@ -10,77 +13,18 @@ public class Arena : SmartContract
         BattleOwner = Message.Sender;
     }
 
+    /// <summary>
+    /// Set the address deploying the contract as battle owner
+    /// </summary>
     private Address BattleOwner
     {
         get => PersistentState.GetAddress(nameof(BattleOwner));
         set => PersistentState.SetAddress(nameof(BattleOwner), value);
     }
 
-    private void SetUser(long battleid, Address address, BattleUser user)
-    {
-        PersistentState.SetStruct($"user:{battleid}{address}", user);
-    }
-
-    private BattleUser GetUser(long battleid, Address address)
-    {
-        return PersistentState.GetStruct<BattleUser>($"user:{battleid}{address}");
-    }
-
-    private void SetBattle(long battleid, BattleMain battle)
-    {
-        PersistentState.SetStruct($"battle:{battleid}", battle);
-    }
-
-    private BattleMain GetBattle(long battleid)
-    {
-        return PersistentState.GetStruct<BattleMain>($"battle:{battleid}");
-    }
-
-    private void ProcessWinner(BattleMain battle)
-    {
-        bool AllScoresSubmitted = true;
-        foreach (Address userAddress in battle.Users)
-        {
-            var user = GetUser(battle.BattleId, userAddress);
-            if (AllScoresSubmitted && !user.ScoreSubmitted)
-                AllScoresSubmitted = false;
-        }
-        if (AllScoresSubmitted)
-        {
-            int winnerIndex = GetWinnerIndex(battle.BattleId, battle.Users);
-            if (battle.WinnerAddress == null || battle.WinnerAddress == Address.Zero)
-            {
-                battle.WinnerAddress = battle.Users[winnerIndex];
-                SetBattle(battle.BattleId, battle);
-                ProcessPrize(battle.BattleId);
-            }
-        }
-    }
-
-    private int GetWinnerIndex(long battleid, Address[] users)
-    {
-        int winningScore = 0;
-        int winningScoreIndex = 0;
-        for (int i = 0; i < users.Length; i++)
-        {
-            var user = GetUser(battleid, users[i]);
-            if (user.Score > winningScore)
-            {
-                winningScore = user.Score;
-                winningScoreIndex = i;
-            }
-        }
-        return winningScoreIndex;
-    }
-
-    private void ProcessPrize(long battleid)
-    {
-        var battle = GetBattle(battleid);
-        ulong prize = battle.Price * (battle.MaxUsers - 1);
-        Transfer(battle.WinnerAddress, prize);
-        Transfer(BattleOwner, battle.Price);
-    }
-
+    /// <summary>
+    /// Battle owner will start the battle
+    /// </summary>
     public bool Start(long battleId, uint maxUsers, ulong price)
     {
         Assert(Message.Sender == BattleOwner, "Only battle owner can start game.");
@@ -95,6 +39,9 @@ public class Arena : SmartContract
         return true;
     }
 
+    /// <summary>
+    /// 4 different user will enter the battle
+    /// </summary>
     public bool EnterGame(long battleId, int userindex)
     {
         var battle = GetBattle(battleId);
@@ -115,6 +62,9 @@ public class Arena : SmartContract
         return true;
     }
 
+    /// <summary>
+    /// 4 different user will end the battle and submit the score
+    /// </summary>
     public bool EndGame(Address userAddress, long battleId, int score)
     {
         Assert(Message.Sender == BattleOwner, "Only battle owner can end game.");
@@ -137,11 +87,88 @@ public class Arena : SmartContract
         return true;
     }
 
+    /// <summary>
+    /// Get winner address
+    /// </summary>
     public Address GetWinner(long battleId)
     {
         Assert(Message.Sender == BattleOwner, "Only battle owner can give right to play.");
         var battle = GetBattle(battleId);
         return battle.WinnerAddress;
+    }
+
+    /// <summary>
+    /// Process winner when all user scores are submitted
+    /// </summary>
+    private void ProcessWinner(BattleMain battle)
+    {
+        bool AllScoresSubmitted = true;
+        foreach (Address userAddress in battle.Users)
+        {
+            var user = GetUser(battle.BattleId, userAddress);
+            if (AllScoresSubmitted && !user.ScoreSubmitted)
+                AllScoresSubmitted = false;
+        }
+        if (AllScoresSubmitted)
+        {
+            int winnerIndex = GetWinnerIndex(battle.BattleId, battle.Users);
+            if (battle.WinnerAddress == null || battle.WinnerAddress == Address.Zero)
+            {
+                battle.WinnerAddress = battle.Users[winnerIndex];
+                SetBattle(battle.BattleId, battle);
+                ProcessPrize(battle.BattleId);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Get winner user index from battle users
+    /// </summary>
+    private int GetWinnerIndex(long battleid, Address[] users)
+    {
+        int winningScore = 0;
+        int winningScoreIndex = 0;
+        for (int i = 0; i < users.Length; i++)
+        {
+            var user = GetUser(battleid, users[i]);
+            if (user.Score > winningScore)
+            {
+                winningScore = user.Score;
+                winningScoreIndex = i;
+            }
+        }
+        return winningScoreIndex;
+    }
+
+    /// <summary>
+    /// Send 3/4 amount to winner and 1/4 amount to battle owner
+    /// </summary>
+    private void ProcessPrize(long battleid)
+    {
+        var battle = GetBattle(battleid);
+        ulong prize = battle.Price * (battle.MaxUsers - 1);
+        Transfer(battle.WinnerAddress, prize);
+        Transfer(BattleOwner, battle.Price);
+    }
+
+    private void SetUser(long battleid, Address address, BattleUser user)
+    {
+        PersistentState.SetStruct($"user:{battleid}{address}", user);
+    }
+
+    private BattleUser GetUser(long battleid, Address address)
+    {
+        return PersistentState.GetStruct<BattleUser>($"user:{battleid}{address}");
+    }
+
+    private void SetBattle(long battleid, BattleMain battle)
+    {
+        PersistentState.SetStruct($"battle:{battleid}", battle);
+    }
+
+    private BattleMain GetBattle(long battleid)
+    {
+        return PersistentState.GetStruct<BattleMain>($"battle:{battleid}");
     }
 
     public struct BattleMain
